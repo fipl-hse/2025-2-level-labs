228439aaf1f93cc92963049c993dc02163d367ea
samx3lee <sammmlee.0@gmail.com>
2025-09-15 14:41:19 +0500

    start

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -3,12 +3,18 @@ Frequency-driven keyword extraction starter
 """
 
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
+import os
 from json import load
+from main import clean_and_tokenize
+from main import check_list
+from main import remove_stop_words
 
+BASE_DIR = os.path.dirname(__file__)
+ASSETS_DIR = os.path.join(BASE_DIR, "assets")
 
 def main() -> None:
     """
-    Launches an implementation.
+    Launches an implementation.  
     """
     with open("assets/Дюймовочка.txt", "r", encoding="utf-8") as file:
         target_text = file.read()
@@ -18,8 +24,12 @@ def main() -> None:
         idf = load(file)
     with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
         corpus_freqs = load(file)
-    result = None
-    assert result, "Keywords are not extracted"
+    result_text = clean_and_tokenize(target_text)
+    result_text = remove_stop_words(result_text, stop_words)
+    result = result_text
+    assert result, "Keywords are not extracted" 
+
+
 
 
 if __name__ == "__main__":
