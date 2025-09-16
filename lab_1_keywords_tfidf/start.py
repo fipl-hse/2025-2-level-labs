b3e91967a0dd56daa50d0908a0522eea8dc34a46
anna_kaizerova <kaizerova.a@yandex.ru>
2025-09-16 16:34:37 +0300

    start file is updated

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -4,11 +4,7 @@ Frequency-driven keyword extraction starter
 
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
 from json import load
-from main import (check_list,
-                  check_dict,
-                  check_positive_int,
-                  check_float,
-                  clean_and_tokenize,
+from main import (clean_and_tokenize,
                   remove_stop_words,
                   calculate_frequencies,
                   get_top_n,
