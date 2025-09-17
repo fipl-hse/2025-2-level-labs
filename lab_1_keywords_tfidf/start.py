5d67a6885656f0d1a29201feafc73687b29685a2
samx3lee <sammmlee.0@gmail.com>
2025-09-17 07:54:41 +0500

    imported functions

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -6,13 +6,16 @@ Frequency-driven keyword extraction starter
 import os
 from json import load
 from main import clean_and_tokenize
+from main import check_dict
 from main import check_list
 from main import remove_stop_words
+from main import calculate_frequencies
+from main import get_top_n
 
 BASE_DIR = os.path.dirname(__file__)
 ASSETS_DIR = os.path.join(BASE_DIR, "assets")
 
-def main() -> None:
+def main() -> None: 
     """
     Launches an implementation.  
     """
@@ -24,8 +27,10 @@ def main() -> None:
         idf = load(file)
     with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
         corpus_freqs = load(file)
+
     result_text = clean_and_tokenize(target_text)
     result_text = remove_stop_words(result_text, stop_words)
+    result_text = calculate_frequencies(result_text)
     result = result_text
     assert result, "Keywords are not extracted" 
 
