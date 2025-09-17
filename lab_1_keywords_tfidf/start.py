e2c024d0f3cd362c48863bca3c099f0f8995abba
Olga <olgakuznetsova006@gmail.com>
2025-09-17 13:31:03 +0300

    last last try

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -6,13 +6,10 @@ Frequency-driven keyword extraction starter
 from json import load
 
 from lab_1_keywords_tfidf.main import (
-    calculate_chi_values,
-    calculate_expected_frequency,
     calculate_frequencies,
     calculate_tf,
     calculate_tfidf,
     clean_and_tokenize,
-    extract_significant_words,
     get_top_n,
     remove_stop_words,
 )
@@ -22,6 +19,10 @@ def main() -> None:
     """
     Launches an implementation.
     """
+    cleaned_tokens = None
+    frequencies = None
+    tf_values = None
+    tfidf_values = None
     with open("assets/Дюймовочка.txt", "r", encoding="utf-8") as file:
         target_text = file.read()
     tokens = clean_and_tokenize(target_text)
