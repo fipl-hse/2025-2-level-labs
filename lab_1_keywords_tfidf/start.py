b851fe7cbb55c7bb6ce078d8cf0ecbe49c392a61
Olga <olgakuznetsova006@gmail.com>
2025-09-17 11:08:45 +0300

    change import

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -5,8 +5,8 @@ Frequency-driven keyword extraction starter
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
 from json import load
 
-from main import calculate_frequencies, calculate_tf, calculate_tfidf
 from main import clean_and_tokenize, remove_stop_words
+from main import calculate_frequencies, calculate_tf, calculate_tfidf
 
 
 def main() -> None:
@@ -15,18 +15,18 @@ def main() -> None:
     """
     with open("assets/Дюймовочка.txt", "r", encoding="utf-8") as file:
         target_text = file.read()
-        tokens = clean_and_tokenize(target_text)
+    tokens = clean_and_tokenize(target_text)
     with open("assets/stop_words.txt", "r", encoding="utf-8") as file:
         stop_words = file.read().split("\n")
-        cleaned_tokens = remove_stop_words(tokens, stop_words)
+    cleaned_tokens = remove_stop_words(tokens, stop_words)
+    frequencies = calculate_frequencies(cleaned_tokens)
+    tf_values = calculate_tf(frequencies)
     with open("assets/IDF.json", "r", encoding="utf-8") as file:
         idf = load(file)
-        frequencies = calculate_frequencies(cleaned_tokens)
-        tf_values = calculate_tf(frequencies)
-        idf_values = calculate_tfidf(tf_values, idf)
+    tfidf_values = calculate_tfidf(tf_values, idf)
     with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
         corpus_freqs = load(file)
-    result = cleaned_tokens
+    result = tfidf_values
     assert result, "Keywords are not extracted"
 
 
