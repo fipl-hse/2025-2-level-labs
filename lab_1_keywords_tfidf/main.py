54a289ae33c2df5e586057443b1273a85af9d303
Jonquilot <steshovaasa@gmail.com>
2025-09-18 03:21:14 +0300

    8 functions defined, raw algorithm added

--- lab_1_keywords_tfidf/main.py
+++ lab_1_keywords_tfidf/main.py
@@ -20,6 +20,16 @@ def check_list(user_input: Any, elements_type: type, can_be_empty: bool) -> bool
     Returns:
         bool: True if valid, False otherwise
     """
+    if not isinstance(user_input, list):
+        return False
+    
+    if not can_be_empty and not user_input:
+        return False
+    
+    for element in user_input:
+        if not isinstance(element, elements_type):
+            return False
+    return True
 
 
 def check_dict(user_input: Any, key_type: type, value_type: type, can_be_empty: bool) -> bool:
@@ -35,6 +45,18 @@ def check_dict(user_input: Any, key_type: type, value_type: type, can_be_empty: 
     Returns:
         bool: True if valid, False otherwise
     """
+    if not isinstance(user_input, dict):
+        return False
+    
+    if not can_be_empty and not user_input:
+        return False
+
+    for key, value in user_input:
+        if not isinstance(key, key_type):
+            return False
+        if not isinstance(value, value_type):
+            return False
+    return True
 
 
 def check_positive_int(user_input: Any) -> bool:
@@ -47,6 +69,7 @@ def check_positive_int(user_input: Any) -> bool:
     Returns:
         bool: True if valid, False otherwise
     """
+    return isinstance(user_input, int) and not isinstance(user_input, bool) and user_input > 0
 
 
 def check_float(user_input: Any) -> bool:
@@ -59,6 +82,7 @@ def check_float(user_input: Any) -> bool:
     Returns:
         bool: True if valid, False otherwise
     """
+    return isinstance(user_input, float)
 
 
 def clean_and_tokenize(text: str) -> list[str] | None:
@@ -72,6 +96,18 @@ def clean_and_tokenize(text: str) -> list[str] | None:
         list[str] | None: A list of lowercase tokens without punctuation.
         In case of corrupt input arguments, None is returned.
     """
+    if not isinstance(text, str):
+        return None
+    
+    text_without_punctuation = ""
+
+    for char in text:
+        if char.isalpha() or char.isspace():
+            text_without_punctuation += char
+
+    text_cleaned = text_without_punctuation.lower()
+    tokens = text_cleaned.split()
+    return tokens
 
 
 def remove_stop_words(tokens: list[str], stop_words: list[str]) -> list[str] | None:
@@ -86,6 +122,15 @@ def remove_stop_words(tokens: list[str], stop_words: list[str]) -> list[str] | N
         list[str] | None: Token sequence without stop words.
         In case of corrupt input arguments, None is returned.
     """
+    if not isinstance(tokens, list) or not isinstance(stop_words, list):
+        return None
+    
+    if not any(element in tokens for element in stop_words):
+        return None
+    
+    tokens_without_stop_words = [element for element in tokens if element not in stop_words]
+    return tokens_without_stop_words
+
 
 
 def calculate_frequencies(tokens: list[str]) -> dict[str, int] | None:
@@ -99,6 +144,14 @@ def calculate_frequencies(tokens: list[str]) -> dict[str, int] | None:
         dict[str, int] | None: A dictionary {token: occurrences}.
         In case of corrupt input arguments, None is returned.
     """
+    if not isinstance(tokens, list):
+        return None
+    
+    frequencies_dictionary = {}
+
+    for token in tokens:
+        frequencies_dictionary[token] = tokens.count(token)
+    return frequencies_dictionary
 
 
 def get_top_n(frequencies: dict[str, int | float], top: int) -> list[str] | None:
@@ -114,6 +167,14 @@ def get_top_n(frequencies: dict[str, int | float], top: int) -> list[str] | None
         list[str] | None: Top-N tokens sorted by frequency.
         In case of corrupt input arguments, None is returned.
     """
+    if not isinstance(frequencies, dict) or not isinstance(top, int):
+        return None
+    
+    frequencies_sorted_items = sorted(frequencies.items(), key=lambda item: item[1], reverse=True)
+    frequencies_sorted_items_top = frequencies_sorted_items[:top]
+    frequencies_top_n = dict(frequencies_sorted_items)
+    return frequencies_top_n
+
 
 
 def calculate_tf(frequencies: dict[str, int]) -> dict[str, float] | None:
--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -4,6 +4,7 @@ Frequency-driven keyword extraction starter
 
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
 from json import load
+from main import check_dict, check_float, check_list, check_positive_int, clean_and_tokenize, remove_stop_words, calculate_frequencies, get_top_n
 
 
 def main() -> None:
@@ -19,6 +20,13 @@ def main() -> None:
     with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
         corpus_freqs = load(file)
     result = None
+
+    text_tokenized = clean_and_tokenize(target_text)
+    tokens_without_stop_words = remove_stop_words(text_tokenized, stop_words)
+    frequencies_dictionary = calculate_frequencies(tokens_without_stop_words)
+    top_n_tokens = get_top_n(frequencies_dictionary, 10)
+
+    print(top_n_tokens)
     assert result, "Keywords are not extracted"
 
 
