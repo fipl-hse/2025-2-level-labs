838f2dc82e93d9fc70e04f7c063a24fe69cf202d
Асташкина Александра <aleksasha.ast@gmail.com>
2025-09-23 17:38:25 +0300

    start.py

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -5,6 +5,16 @@ Frequency-driven keyword extraction starter
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
 from json import load
 
+from main import clean_and_tokenize
+
+from main import remove_stop_words
+
+from main import calculate_frequencies
+
+from main import get_top_n
+
+from main import calculate_tf
+
 
 def main() -> None:
     """
@@ -18,7 +28,17 @@ def main() -> None:
         idf = load(file)
     with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
         corpus_freqs = load(file)
-    result = None
+    unfilt_tokens = clean_and_tokenize(target_text)
+    print(unfilt_tokens)
+    tokens = remove_stop_words(unfilt_tokens, stop_words)
+    print (tokens)
+    frequencies = calculate_frequencies(tokens)
+    print (frequencies)
+    top_values = get_top_n(frequencies)
+    print (top_values)
+    term_freq = calculate_tf(frequencies)
+    print (term_freq)
+    result = term_freq #(изменить в 12 шаге)
     assert result, "Keywords are not extracted"
 
 
