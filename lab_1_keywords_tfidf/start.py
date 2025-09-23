af5e253173edd4262b9523ce2be7ef53e8213fb7
Polina Kostusenko <pakosrusenko@edu.hse.ru>
2025-09-23 14:02:13 +0300

    kf

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -2,38 +2,61 @@
 Frequency-driven keyword extraction starter
 """
 
+# from pathlib import Path
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
-from main import clean_and_tokenize
-from main import remove_stop_words
-from main import calculate_frequencies
-from main import get_top_n
-from main import calculate_tf
-from main import calculate_tfidf
-from main import calculate_expected_frequency
-from main import calculate_chi_values
-from main import extract_significant_words
-
 from json import load
+
+from main import (  # calculate_chi_values,; calculate_expected_frequency,; extract_significant_words,
+    calculate_frequencies,
+    calculate_tf,
+    calculate_tfidf,
+    clean_and_tokenize,
+    get_top_n,
+    remove_stop_words,
+)
 
 
 def main() -> None:
     """
     Launches an implementation.
     """
-    with open("assets/Дюймовочка.txt", "r", encoding="utf-8") as file:
+    with open(
+        r"C:\Users\polin\SuperSlay\2025-2-level-labs\lab_1_keywords_tfidf\assets\Дюймовочка.txt",
+        "r",
+        encoding="utf-8",
+    ) as file:
         target_text = file.read()
-    with open("assets/stop_words.txt", "r", encoding="utf-8") as file:
+    with open(
+        r"C:\Users\polin\SuperSlay\2025-2-level-labs\lab_1_keywords_tfidf\assets\stop_words.txt",
+        "r",
+        encoding="utf-8",
+    ) as file:
         stop_words = file.read().split("\n")
-    with open("assets/IDF.json", "r", encoding="utf-8") as file:
+    with open(
+        r"C:\Users\polin\SuperSlay\2025-2-level-labs\lab_1_keywords_tfidf\assets\IDF.json",
+        "r",
+        encoding="utf-8",
+    ) as file:
         idf = load(file)
-    with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
+    with open(
+        r"C:\Users\polin\SuperSlay\2025-2-level-labs\lab_1_keywords_tfidf\assets\corpus_frequencies.json",
+        "r",
+        encoding="utf-8",
+    ) as file:
         corpus_freqs = load(file)
+
+    tokens = clean_and_tokenize(target_text)
+    wo_stop_words = remove_stop_words(tokens, stop_words)
+    frequencies = calculate_frequencies(wo_stop_words)
+    top_n_1 = get_top_n(frequencies, 10)
+    term_frequencies = calculate_tf(frequencies)
+    tf_idf = calculate_tfidf(term_frequencies, idf)
+    top_n_2 = get_top_n(tf_idf, 10)
+    print(top_n_2)
+
     #result = None
     #assert result, "Keywords are not extracted"
 
-    tokens = clean_and_tokenize(target_text)
-    print(f"Tokens (first 20): {tokens[:20]}")
 
 if __name__ == "__main__":
     main()
-
