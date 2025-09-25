4ce1e601d071051caf56f1e9875697bf77a2e47d
a <yaraketka@gmail.com>
2025-09-25 13:00:23 +0300

    Everything for 10 with sad mypy

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -31,10 +31,11 @@ def main() -> None:
     result = get_top_n((calculate_tfidf(calculate_tf(calculate_frequencies \
         (remove_stop_words(clean_and_tokenize(target_text), stop_words))), idf)), 10)
     print(result)
-    result = extract_significant_words(calculate_chi_values(calculate_expected_frequency \
+    result = get_top_n(extract_significant_words(calculate_chi_values(calculate_expected_frequency \
         (calculate_frequencies(remove_stop_words(clean_and_tokenize(target_text), stop_words)), \
         corpus_freqs), calculate_frequencies (remove_stop_words(clean_and_tokenize \
-        (target_text), stop_words))), 0.05)
+        (target_text), stop_words))), 0.05), 10)
+    print(result)
     assert result, "Keywords are not extracted"
 
 
