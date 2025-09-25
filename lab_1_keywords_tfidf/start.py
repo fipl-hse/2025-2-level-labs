a7536cb5b293b33d102967a606dbcf8a43049cae
Mikhaki <mh4waretgirl@gmail.com>
2025-09-25 18:57:43 +0300

    solving mypy and style problems

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -30,24 +30,31 @@ def main() -> None:
         idf = load(file)
     with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
         corpus_freqs = load(file)
-    if target_text:
-        tokenized_text = clean_and_tokenize(target_text)
-    if tokenized_text:
-        text_without_stopwords = remove_stop_words(tokenized_text, stop_words)
-    if text_without_stopwords:
-        frequ_dict = calculate_frequencies(text_without_stopwords)
-    if frequ_dict:
-        tf_dict = calculate_tf(frequ_dict)
-    if tf_dict:
-        tfidf_dict = calculate_tfidf(tf_dict, idf)
-    if tf_dict and tfidf_dict:
-        expected_freq = calculate_expected_frequency(frequ_dict, corpus_freqs)
-    if expected_freq and tfidf_dict:
-        chi_values = calculate_chi_values(expected_freq, tfidf_dict)
-    if chi_values:
-        significant_words = extract_significant_words(chi_values, 0.01)
-    if significant_words:
-        top_significant_words = get_top_n(significant_words, 10)
+    tokenized_text = clean_and_tokenize(target_text)
+    if not tokenized_text:
+        return None
+    text_without_stopwords = remove_stop_words(tokenized_text, stop_words)
+    if not tokenized_text:
+        return None
+    frequ_dict = calculate_frequencies(text_without_stopwords)
+    if not frequ_dict:
+        return None
+    tf_dict = calculate_tf(frequ_dict)
+    if not tf_dict:
+        return None
+    tfidf_dict = calculate_tfidf(tf_dict, idf)
+    if not tf_dict:
+        return None
+    expected_freq = calculate_expected_frequency(frequ_dict, corpus_freqs)
+    if not expected_freq:
+        return None
+    chi_values = calculate_chi_values(expected_freq, tfidf_dict)
+    if not chi_values:
+        return None
+    significant_words = extract_significant_words(chi_values, 0.01)
+    if not significant_words:
+        return None
+    top_significant_words = get_top_n(significant_words, 10)
     result = top_significant_words
 
     print(result)
