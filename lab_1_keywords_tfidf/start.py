3d6358ba093616061b70adf686668a4ac08906e1
anastasianamakonova <anastasianamakonova@yandex.ru>
2025-09-26 01:14:07 +0300

    added changes for CI

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -2,12 +2,20 @@
 Frequency-driven keyword extraction starter
 """
 
-# pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
-from json import load
+import json
 
-from main import (clean_and_tokenize, remove_stop_words, calculate_chi_values,
-                  calculate_expected_frequency, calculate_frequencies, calculate_tf,
-                  calculate_tfidf, extract_significant_words, get_top_n)
+from main import (
+    calculate_chi_values,
+    calculate_expected_frequency,
+    calculate_frequencies,
+    calculate_tf,
+    calculate_tfidf,
+    clean_and_tokenize,
+    extract_significant_words,
+    get_top_n,
+    remove_stop_words,
+)
+
 
 def main() -> None:
     """
@@ -22,12 +30,12 @@ def main() -> None:
         freq_dict = calculate_frequencies(cleaned_tokens)
         get_top_words = get_top_n(freq_dict, 5)
     with open("assets/IDF.json", "r", encoding="utf-8") as file:
-        idf = load(file)
+        idf = json.load(file)
         tf = calculate_tf(freq_dict)
         tfidf = calculate_tfidf(tf, idf)
         top_10_tfidf = get_top_n(tfidf, 10)
     with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
-        corpus_freqs = load(file)
+        corpus_freqs = json.load(file)
         expected_freqs = calculate_expected_frequency(freq_dict, corpus_freqs)
         chi_vals = calculate_chi_values(expected_freqs, freq_dict)
         significant_words = extract_significant_words(chi_vals, 0.05)
@@ -39,4 +47,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-    
