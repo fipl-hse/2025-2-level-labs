21305535518fc260437d3799f8098ad3f52241ea
a <yaraketka@gmail.com>
2025-09-25 15:26:11 +0300

    Will you tell me too many returns

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -49,6 +49,8 @@ def main() -> None:
     result = get_top_n(significant_words, 10)
     print(result)
     term_freq = calculate_tf(frequencies)
+    if not term_freq:
+        return
     tf_idf = calculate_tfidf(term_freq, idf)
     print(term_freq, tf_idf)
     assert result, "Keywords are not extracted"
