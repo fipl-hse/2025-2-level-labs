89051e82623446aefc0bb32df0b6f79d9e347cf2
arigato4pts <danilakuznet@mail.ru>
2025-09-25 13:17:19 +0300

    ahoy fix me start.py 6 harrr

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -3,8 +3,8 @@ Frequency-driven keyword extraction starter
 """
 
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
-from json import load
 
+from json import load
 
 from lab_1_keywords_tfidf.main import(
     calculate_chi_values,
@@ -25,17 +25,12 @@ def main() -> None:
     """
     with open("assets/Дюймовочка.txt", "r", encoding="utf-8") as file:
         target_text = file.read()
-
     with open("assets/stop_words.txt", "r", encoding="utf-8") as file:
         stop_words = file.read().split("\n")
-
     with open("assets/IDF.json", "r", encoding="utf-8") as file:
         idf = load(file)
-
     with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
         corpus_freqs = load(file)
-
-
     tokens = clean_and_tokenize(target_text) or []
     wo_stop_words = remove_stop_words(tokens, stop_words) or []
     frequencies = calculate_frequencies(wo_stop_words) or {}
@@ -46,9 +41,10 @@ def main() -> None:
     significant_words = extract_significant_words(chi_values, 0.001) or {}
     top_n_3 = get_top_n(significant_words, 10) or []
     print(top_n_3)
-
     # result = None
     # assert result, "Keywords are not extracted"
+
+
 
 
 if __name__ == "__main__":
