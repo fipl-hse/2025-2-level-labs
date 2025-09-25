20aa6cda0838a7be50141d336490d1e412a33db8
anastasianamakonova <anastasianamakonova@yandex.ru>
2025-09-26 00:45:55 +0300

    added changes for 10 score

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -4,14 +4,10 @@ Frequency-driven keyword extraction starter
 
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
 from json import load
-from main import (
-    clean_and_tokenize,
-    remove_stop_words,
-    calculate_frequencies,
-    calculate_tf,
-    calculate_tfidf,
-    get_top_n
-)
+
+from main import (clean_and_tokenize, remove_stop_words, calculate_chi_values,
+                  calculate_expected_frequency, calculate_frequencies, calculate_tf,
+                  calculate_tfidf, extract_significant_words, get_top_n)
 
 def main() -> None:
     """
@@ -30,12 +26,17 @@ def main() -> None:
         tf = calculate_tf(freq_dict)
         tfidf = calculate_tfidf(tf, idf)
         top_10_tfidf = get_top_n(tfidf, 10)
-        print(top_10_tfidf)
     with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
         corpus_freqs = load(file)
+        expected_freqs = calculate_expected_frequency(freq_dict, corpus_freqs)
+        chi_vals = calculate_chi_values(expected_freqs, freq_dict)
+        significant_words = extract_significant_words(chi_vals, 0.05)
+        top_chi = get_top_n(significant_words, 10) if significant_words else []
+        print("Топ-10 ключевых слов по хи-квадрат:", top_chi)
     result = cleaned_tokens
     assert result, "Keywords are not extracted"
 
 
 if __name__ == "__main__":
     main()
+    
