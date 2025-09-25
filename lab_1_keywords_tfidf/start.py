23541e94d935000660b86c8fe4e5f1d8729a12a7
arigato4pts <danilakuznet@mail.ru>
2025-09-25 11:38:21 +0300

    fix start.py 2

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -7,13 +7,15 @@ from json import load
 
 
 from lab_1_keywords_tfidf.main import(
+    calculate_chi_values,
     calculate_expected_frequency,
     calculate_frequencies,
     calculate_tf,
     calculate_tfidf,
     clean_and_tokenize,
+    extract_significant_words,
     get_top_n,
-    remove_stop_words
+    remove_stop_words,
 )
 def main() -> None:
     """
@@ -30,7 +32,16 @@ def main() -> None:
     result = None
     assert result, "Keywords are not extracted"
 
-
+    tokens = clean_and_tokenize(target_text) or []
+    wo_stop_words = remove_stop_words(tokens, stop_words) or []
+    frequencies = calculate_frequencies(wo_stop_words) or {}
+    term_frequencies = calculate_tf(frequencies) or {}
+    tf_idf = calculate_tfidf(term_frequencies, idf) or {}
+    expected = calculate_expected_frequency(frequencies, corpus_freqs) or {}
+    chi_values = calculate_chi_values(expected, frequencies) or {}
+    significant_words = extract_significant_words(chi_values, 0.001) or {}
+    top_n_3 = get_top_n(significant_words, 10) or []
+    print(top_n_3)
 
 if __name__ == "__main__":
     main()
