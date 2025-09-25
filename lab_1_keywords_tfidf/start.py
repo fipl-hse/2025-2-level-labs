1f8a7136303383213d3a309966e5778783d9a9f8
SletA <sletanimatics@gmail.com>
2025-09-25 12:58:28 +0300

    newCommingFixCodeFive

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -4,8 +4,17 @@ Frequency-driven keyword extraction starter
 
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
 from json import load
-from lab_1_keywords_tfidf.main import clean_and_tokenize, get_top_n, calculate_frequencies, calculate_expected_frequency, calculate_tf, calculate_tfidf, remove_stop_words, extract_significant_words
-
+from main import (
+    calculate_chi_values,
+    calculate_expected_frequency,
+    calculate_frequencies,
+    calculate_tf,
+    calculate_tfidf,
+    clean_and_tokenize,
+    extract_significant_words,
+    get_top_n,
+    remove_stop_words
+)
 def main() -> None:
     """
     Launches an implementation.
@@ -18,17 +27,16 @@ def main() -> None:
         idf = load(file)
     with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
         corpus_freqs = load(file)
-    # result = None
-    # assert result, "Keywords are not extracted"
-    cleaned_text = clean_and_tokenize(target_text)
-    cleaned_text = remove_stop_words(cleaned_text, stop_words)
-    text_frequencies = calculate_frequencies(cleaned_text)
-    tf_frequencies = calculate_tf(text_frequencies)
-    tfidf_frequencies = calculate_tfidf(tf_frequencies, idf)
-    expected_frequencies = calculate_expected_frequency(text_frequencies, corpus_freqs)
-    only_key_words = extract_significant_words(tfidf_frequencies)
-    top_words = get_top_n(only_key_words, 10)
-    result = top_words
+    result = None
+    if not target_text == None and not stop_words == None and not idf == None and not corpus_freqs == None:
+        cleaned_text = clean_and_tokenize(target_text)
+        cleaned_text = remove_stop_words(cleaned_text, stop_words)
+        text_frequencies = calculate_frequencies(cleaned_text)
+        tf_frequencies = calculate_tf(text_frequencies)
+        tfidf_frequencies = calculate_tfidf(tf_frequencies, idf)
+        expected_frequencies = calculate_expected_frequency(text_frequencies, corpus_freqs)
+        only_key_words = extract_significant_words(tfidf_frequencies)
+        top_words = get_top_n(only_key_words, 10)
     assert result, "Keywords are not extracted"
 
 
