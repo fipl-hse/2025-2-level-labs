0fbfebc661b3f417f05d349508ec50a3f655076a
Асташкина Александра <aleksasha.ast@gmail.com>
2025-09-27 09:49:31 +0300

    code style??

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -6,10 +6,13 @@ Frequency-driven keyword extraction starter
 from json import load
 
 from lab_1_keywords_tfidf.main import (
+    calculate_chi_values,
+    calculate_expected_frequency,
     calculate_frequencies,
     calculate_tf,
     calculate_tfidf,
     clean_and_tokenize,
+    extract_significant_words,
     get_top_n,
     remove_stop_words,
 )
@@ -33,8 +36,6 @@ def main() -> None:
     print (tokens)
     frequencies = calculate_frequencies(tokens)
     print (frequencies)
-    top_tokens = get_top_n(frequencies)
-    print (top_tokens)
     term_freq = calculate_tf(frequencies)
     print (term_freq)
     tfidf_dict = calculate_tfidf(term_freq, idf)
