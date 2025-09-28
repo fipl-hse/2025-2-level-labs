bbe27e8d6ae415ba96c35ba7e728a178b2b45e22
Арина <kretoarina0012@gmail.com>
2025-09-29 00:14:03 +0300

    start

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -3,7 +3,9 @@ Frequency-driven keyword extraction starter
 """
 
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
+
 from json import load
+
 
 from lab_1_keywords_tfidf.main import (
     calculate_frequencies,
@@ -11,6 +13,7 @@ from lab_1_keywords_tfidf.main import (
     get_top_n,
     remove_stop_words,
 )
+
 
 def main() -> None:
     """
