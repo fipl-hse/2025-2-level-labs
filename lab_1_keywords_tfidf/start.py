0a53c627599cf2b50e861c3744d9f04b0c3b59f7
Yaroslava <yaroslavalukacheva@gmail.com>
2025-09-29 01:38:31 +0300

    new return

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -29,17 +29,29 @@ def main() -> None:
     with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
         corpus_freqs = load(file)
 
-    cleaned_text=clean_and_tokenize(target_text) or []
-    without_stop_words=remove_stop_words(cleaned_text, stop_words) or []
-    calculated_frequencies = calculate_frequencies(without_stop_words) or {}
-    expected_frequency=calculate_expected_frequency(calculated_frequencies, corpus_freqs) or {}
-    chi_values=calculate_chi_values(expected_frequency, calculated_frequencies) or {}
-    significant_words=extract_significant_words(chi_values, 0.05) or {}
-    get_top_words=get_top_n(significant_words, 10) or []
-
+    cleaned_text=clean_and_tokenize(target_text)
+    if clean_and_tokenize is None:
+        return
+    without_stop_words=remove_stop_words(cleaned_text, stop_words)
+    if without_stop_words is None:
+        return
+    calculated_frequencies = calculate_frequencies(without_stop_words)
+    if calculated_frequencies is None:
+        return
+    expected_frequency=calculate_expected_frequency(calculated_frequencies, corpus_freqs)
+    if expected_frequency is None:
+        return
+    chi_values=calculate_chi_values(expected_frequency, calculated_frequencies)
+    if chi_values is None:
+        return
+    significant_words=extract_significant_words(chi_values, 0.05)
+    if significant_words is None:
+        return
+    get_top_words=get_top_n(significant_words, 10)
     result=get_top_words
-    print(result)
+
     assert result, "Keywords are not extracted"
+    print(result)
 
 if __name__ == "__main__":
     main()
