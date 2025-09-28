b5df394eeb56f9e29bc5a1851d1614cbcee96f18
NatalieSmirnova7 <nataliasmirnova2006@gmail.com>
2025-09-28 19:04:59 +0300

    Upd

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -4,8 +4,7 @@ Frequency-driven keyword extraction starter
 
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
 
-from json import load
-    
+from json import load    
 from lab_1_keywords_tfidf.main import (
     calculate_frequencies,
     calculate_tf,
