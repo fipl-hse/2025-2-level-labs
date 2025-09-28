3ef5b204e780419fe71d560b74b92403e0fb3d66
Polina Kostusenko <pakosrusenko@edu.hse.ru>
2025-09-28 22:33:08 +0300

    jgkfl

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -36,22 +36,25 @@ def main() -> None:
         corpus_freqs = load(file)
 
     tokens = clean_and_tokenize(target_text) or []
-    tokens_without_stopwords = remove_stop_words(tokens, stop_words) or []
-    print(tokens_without_stopwords)
-    frequencies = calculate_frequencies(tokens_without_stopwords) or {}
-    term_freq_tf = calculate_tf(frequencies) or {}
-    print(term_freq_tf)
-    term_freq_tfidf = calculate_tfidf(term_freq_tf, idf) or {}
-    print(term_freq_tfidf)
-    top_key_words = get_top_n(term_freq_tfidf, 10) or []
-    print(', '.join(top_key_words))
+    wo_stop_words = remove_stop_words(tokens, stop_words) or []
+    frequencies = calculate_frequencies(wo_stop_words) or {}
+
+    get_top_n_1 = get_top_n(cast(dict[str, int | float], frequencies), 10)
+
+    term_frequencies = calculate_tf(frequencies) or {}
+    tf_idf = calculate_tfidf(term_frequencies, idf) or {}
+
+    get_top_n_2 = get_top_n(tf_idf, 10) or []
+    print(get_top_n_2)
+
     expected = calculate_expected_frequency(frequencies, corpus_freqs) or {}
     chi_values = calculate_chi_values(expected, frequencies) or {}
-    significant_words = extract_significant_words(chi_values, alpha=0.001) or {}
-    print(significant_words)
-    key_words_chi = get_top_n(chi_values, 10) or []
-    print(', '.join(key_words_chi))
-    result = key_words_chi
+
+    significant_words = extract_significant_words(chi_values, 0.001) or {}
+    get_top_n_3 = get_top_n(significant_words, 10) or [
+    print(get_top_n_3)
+
+    result = get_top_n
     assert result, "Keywords are not extracted"
 
 
