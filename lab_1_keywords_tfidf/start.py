9a64a2d6ccec687882b70fa59169c649a95b59dc
anastasianamakonova <anastasianamakonova@yandex.ru>
2025-09-28 17:43:20 +0300

    added changes for CI 10

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -25,42 +25,25 @@ def main() -> None:
         with open("assets/Дюймовочка.txt", "r", encoding="utf-8") as file:
             target_text = file.read()
         tokens = clean_and_tokenize(target_text)
-        if not tokens:
-            print("Ошибка на этапе предобработки текста")
-            return
         with open("assets/stop_words.txt", "r", encoding="utf-8") as file:
             stop_words = file.read().split("\n")
         cleaned_tokens = remove_stop_words(tokens, stop_words)
-        if not cleaned_tokens:
-            print("Ошибка на этапе удаления стоп-слов")
-            return
         freq_dict = calculate_frequencies(cleaned_tokens)
-        if not freq_dict:
-            print("Ошибка на этапе предобработки текста")
-            return
         with open("assets/IDF.json", "r", encoding="utf-8") as file:
             idf = json.load(file)
         with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
             corpus_freqs = json.load(file)
         tf = calculate_tf(freq_dict)
-        if not tf:
-            print("Ошибка в расчетах TF")
-            return
         tfidf = calculate_tfidf(tf, idf)
-        if not tfidf:
-            print("Ошибка в расчетах TF-IDF")
-            return
         expected_freqs = calculate_expected_frequency(freq_dict, corpus_freqs)
-        if not expected_freqs:
-            print("Ошибка в расчетах ожидаемых частот")
-            return
         chi_vals = calculate_chi_values(expected_freqs, freq_dict)
-        if not chi_vals:
-            print("Ошибка в расчетах статистик")
-            return
         significant_words = extract_significant_words(chi_vals, 0.05)
         top_chi = get_top_n(significant_words, 10) if significant_words else []
-        print("Топ-10 ключевых слов по хи-квадрат:", top_chi)
+        if (tokens and cleaned_tokens and freq_dict and tf and tfidf and 
+            expected_freqs and chi_vals and significant_words and top_chi):
+            print("Топ-10 ключевых слов по хи-квадрат:", top_chi)
+        else:
+            print("Ошибка в процессе обработки текста")
     except FileNotFoundError as e:
         print(f"Файл не найден: {e}")
 
