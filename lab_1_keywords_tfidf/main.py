e363c781c1a5817e33dd903ed083e87481b244c1
Арина <kretoarina0012@gmail.com>
2025-09-29 11:30:29 +0300

    main

--- lab_1_keywords_tfidf/main.py
+++ lab_1_keywords_tfidf/main.py
@@ -7,6 +7,7 @@ Extract keywords based on frequency related metrics
 # pylint:disable=unused-argument
 from typing import Any
 
+
 def check_list(user_input: Any, elements_type: type, can_be_empty: bool) -> bool:
     """
     Check if the object is a list containing elements of a certain type.
