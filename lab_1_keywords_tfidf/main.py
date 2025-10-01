ffab7b05bae6d563de12376dfb6f3f63039fb20e
EIzharikov <$EIzharikov@users.noreply.github.com>
2025-10-01 12:10:30 +0300

    "Update repository with upstream/main"

--- lab_1_keywords_tfidf/main.py
+++ lab_1_keywords_tfidf/main.py
@@ -4,7 +4,6 @@ Lab 1
 Extract keywords based on frequency related metrics
 """
 
-
 # pylint:disable=unused-argument
 import math
 from typing import Any
--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -4,19 +4,6 @@ Frequency-driven keyword extraction starter
 
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
 from json import load
-from typing import cast
-
-from lab_1_keywords_tfidf.main import (
-    calculate_chi_values,
-    calculate_expected_frequency,
-    calculate_frequencies,
-    calculate_tf,
-    calculate_tfidf,
-    clean_and_tokenize,
-    extract_significant_words,
-    get_top_n,
-    remove_stop_words,
-)
 
 from lab_1_keywords_tfidf.main import (
     calculate_chi_values,
@@ -37,13 +24,10 @@ def main() -> None:
     """
     with open("assets/Дюймовочка.txt", "r", encoding="utf-8") as file:
         target_text = file.read()
-
     with open("assets/stop_words.txt", "r", encoding="utf-8") as file:
         stop_words = file.read().split("\n")
-
     with open("assets/IDF.json", "r", encoding="utf-8") as file:
         idf = load(file)
-
     with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
         corpus_freqs = load(file)
     tokens = clean_and_tokenize(target_text) or []
