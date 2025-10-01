912c3e4b533bdf8aa679c9242410a4650ca79591
EIzharikov <$EIzharikov@users.noreply.github.com>
2025-10-01 12:10:59 +0300

    "Update repository with upstream/main"

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -49,29 +49,6 @@ def main() -> None:
     result = key_words_chi
     assert result, "Keywords are not extracted"
 
-    cleaned_text=clean_and_tokenize(target_text)
-    if cleaned_text is None:
-        return
-    without_stop_words=remove_stop_words(cleaned_text, stop_words)
-    if without_stop_words is None:
-        return
-    calculated_frequencies = calculate_frequencies(without_stop_words)
-    if calculated_frequencies is None:
-        return
-    expected_frequency=calculate_expected_frequency(calculated_frequencies, corpus_freqs)
-    if expected_frequency is None:
-        return
-    chi_values=calculate_chi_values(expected_frequency, calculated_frequencies)
-    if chi_values is None:
-        return
-    significant_words=extract_significant_words(chi_values, 0.05)
-    if significant_words is None:
-        return
-    get_top_words=get_top_n(significant_words, 10)
-    result=get_top_words
-
-    assert result, "Keywords are not extracted"
-    print(result)
 
 if __name__ == "__main__":
     main()
