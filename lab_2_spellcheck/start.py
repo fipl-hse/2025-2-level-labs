c81a813e1cd5e2d80e32f70bc49564187de153ea
arigato4pts <danilakuznet@mail.ru>
2025-10-04 22:05:48 +0300

    some start.py changes reverted

--- lab_2_spellcheck/start.py
+++ lab_2_spellcheck/start.py
@@ -3,10 +3,6 @@ Spellcheck starter
 """
 
 # pylint:disable=unused-variable, duplicate-code, too-many-locals
-
-
-from lab_2_spellcheck.main import *
-
 def main() -> None:
     """
     Launches an implementation.
