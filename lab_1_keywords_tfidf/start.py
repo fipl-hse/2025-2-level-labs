64aa0694112d5e276adb421102e8713b50e812e6
annaklimenko-1 <afklimenko@edu.hse.ru>
2025-10-07 10:21:25 +0300

    ci

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -5,7 +5,7 @@ Frequency-driven keyword extraction starter
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
 from json import load
 
-from main import (
+from .main import (
     calculate_frequencies,
     calculate_tf,
     calculate_tfidf,
