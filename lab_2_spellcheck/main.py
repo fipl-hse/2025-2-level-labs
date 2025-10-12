a50b2806c816f09d5ac20267389a95babea945bf
Olga <olgakuznetsova006@gmail.com>
2025-10-12 17:01:17 +0300

    isort in main.py

--- lab_2_spellcheck/main.py
+++ lab_2_spellcheck/main.py
@@ -5,10 +5,8 @@ Lab 2.
 # pylint:disable=unused-argument
 from typing import Literal
 
-from lab_1_keywords_tfidf.main import (
-    check_list,
-    check_dict
-)
+from lab_1_keywords_tfidf.main import check_dict, check_list
+
 
 def build_vocabulary(tokens: list[str]) -> dict[str, float] | None:
     """
