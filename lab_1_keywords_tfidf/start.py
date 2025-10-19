7ed64764971bcf230ebf54b3e80a3b7eacb38aa9
kieoha <romanovae679@gmail.com>
2025-10-19 20:36:56 +0300

    losing my mind yayyy

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -4,18 +4,6 @@ Frequency-driven keyword extraction starter
 
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
 from json import load
-from main import (
-        calculate_chi_values,
-        calculate_expected_frequency,
-        calculate_frequencies,
-        calculate_tf,
-        calculate_tfidf,
-        clean_and_tokenize,
-        extract_significant_words,
-        get_top_n,
-        remove_stop_words,
-    )
-
 from lab_1_keywords_tfidf.main import (
     calculate_chi_values,
     calculate_expected_frequency,
