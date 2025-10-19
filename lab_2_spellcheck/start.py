69f537f83f1a3506d77744803932ea2aa459f766
Polina Kostusenko <pakosrusenko@edu.hse.ru>
2025-10-19 21:48:58 +0300

    import checks2

--- lab_2_spellcheck/start.py
+++ lab_2_spellcheck/start.py
@@ -7,11 +7,10 @@ from lab_1_keywords_tfidf.main import (
     clean_and_tokenize,
     remove_stop_words,
 )
-
 from lab_2_spellcheck.main import (
     build_vocabulary,
     find_out_of_vocab_words,
-    find_correct_word, 
+    find_correct_word,
 )
 
 def main() -> None:
