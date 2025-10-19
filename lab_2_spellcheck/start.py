5ea9ddcbea92048f9f41848ee8b366fc123a377d
AMaryutina <anna151206maryutina@mail.ru>
2025-10-19 21:43:53 +0300

    4th string is changed

--- lab_2_spellcheck/start.py
+++ lab_2_spellcheck/start.py
@@ -1,6 +1,7 @@
 """
 Spellcheck starter
 """
+
 from lab_1_keywords_tfidf.main import clean_and_tokenize, remove_stop_words
 from lab_2_spellcheck.main import build_vocabulary, find_correct_word, find_out_of_vocab_words
 
