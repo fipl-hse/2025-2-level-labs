956399361311ceee5b97e97b6e94976f4fbde9e6
ViktoriaMylnikova <vdmylnikova@edu.hse.ru>
2025-10-19 11:32:10 +0300

    /

--- lab_2_spellcheck/main.py
+++ lab_2_spellcheck/main.py
@@ -144,19 +144,19 @@ def calculate_distance(
     dictionary: dict[str, float] = {}
     for token in vocabulary:
         if method == "jaccard":
-            distance=calculate_jaccard_distance(first_token, token)  # type: ignore
+            distance=calculate_jaccard_distance(first_token, token)
             if distance is not None:
-                dictionary[token]=distance  # type: ignore
+                dictionary[token]=distance
 
         elif method == "levenshtein":
-            distance = calculate_levenshtein_distance(first_token, token)  # type: ignore
+            distance = calculate_levenshtein_distance(first_token, token)
             if distance is not None:
-                dictionary[token]=distance  # type: ignore
+                dictionary[token]=distance
 
         elif method == "jaro-winkler":
-            distance = calculate_jaro_winkler_distance(first_token, token)  # type: ignore
+            distance = calculate_jaro_winkler_distance(first_token, token)
             if distance is not None:
-                dictionary[token]=distance  # type: ignore
+                dictionary[token]=distance
 
     if not dictionary:
         return None
