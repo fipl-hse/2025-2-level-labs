00f65523d828f4fdea8a3a96530ed7bfbcb8e875
kieoha <romanovae679@gmail.com>
2025-10-19 22:18:34 +0300

    code style please dont whine

--- lab_1_keywords_tfidf/main.py
+++ lab_1_keywords_tfidf/main.py
@@ -153,8 +153,6 @@ def get_top_n(frequencies: dict[str, int | float], top: int) -> list[str] | None
     if not all([isinstance(frequencies, dict), check_positive_int(top), frequencies]):
         return None
 
-    return sorted(frequencies.keys(), key=lambda key: frequencies[key], reverse=True)[:top]
-#пропис через лямбду
     for key, value in frequencies.items():
         if not isinstance(value, (int, float)) or not isinstance(key, str):
             return None
