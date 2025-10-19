bbad991b36a87745e15a968ec6b3ac453103c273
kkkozonova <mnevsky08@mail.ru>
2025-10-19 10:11:59 +0300

    might be fixed

--- lab_2_spellcheck/main.py
+++ lab_2_spellcheck/main.py
@@ -5,7 +5,7 @@ Lab 2.
 # pylint:disable=unused-argument
 from typing import Literal
 
-from lab_1_keywords_tfidf.main import check_dict, check_list, check_positive_int
+from lab_1_keywords_tfidf.main import check_dict, check_list, check_float, check_positive_int
 
 
 def build_vocabulary(tokens: list[str]) -> dict[str, float] | None:
