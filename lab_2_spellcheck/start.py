b4100d38ce7c596c60329848d9e324059bf9465c
anastasianamakonova <anastasianamakonova@yandex.ru>
2025-10-19 03:13:46 +0300

    added changes for ci

--- lab_2_spellcheck/start.py
+++ lab_2_spellcheck/start.py
@@ -4,8 +4,8 @@ Spellcheck starter
 
 # pylint:disable=unused-variable, duplicate-code, too-many-locals
 from lab_2_spellcheck.main import (
-    clean_and_tokenize, 
-    remove_stop_words, 
+    clean_and_tokenize,
+    remove_stop_words,
     build_vocabulary,
     find_correct_word,
     find_out_of_vocab_words
