10ff57f2b7895de6555b18ebb58be26b59a3e564
Teimur1 <teimurmolodoybrodyaga@gmail.com>
2025-10-20 09:10:37 +0300

    Changed start.py for better start from powershell, hope it doesnt broke anything

--- lab_2_spellcheck/start.py
+++ lab_2_spellcheck/start.py
@@ -3,6 +3,7 @@ Spellcheck starter
 """
 
 # pylint:disable=unused-variable, duplicate-code, too-many-locals
+import os
 from lab_1_keywords_tfidf.main import clean_and_tokenize, remove_stop_words
 from lab_2_spellcheck.main import (
     build_vocabulary,
@@ -19,18 +20,46 @@ def main() -> None:
     """
     Launches an implementation.
     """
+    BASE_DIR = os.path.dirname(__file__)
+    assets_path = os.path.join(BASE_DIR, "assets")
     with open(
-        "assets/Master_and_Margarita_chapter1.txt", "r", encoding="utf-8"
+        os.path.join(
+            assets_path, "Master_and_Margarita_chapter1.txt"
+        ),
+        "r",
+        encoding="utf-8"
     ) as file:
         text = file.read()
-    with open("assets/stop_words.txt", "r", encoding="utf-8") as file:
+    with open(
+        os.path.join(assets_path, "stop_words.txt"), "r", encoding="utf-8"
+    ) as file:
         stop_words = file.read().split("\n")
     with (
-        open("assets/incorrect_sentence_1.txt", "r", encoding="utf-8") as f1,
-        open("assets/incorrect_sentence_2.txt", "r", encoding="utf-8") as f2,
-        open("assets/incorrect_sentence_3.txt", "r", encoding="utf-8") as f3,
-        open("assets/incorrect_sentence_4.txt", "r", encoding="utf-8") as f4,
-        open("assets/incorrect_sentence_5.txt", "r", encoding="utf-8") as f5,
+        open(
+            os.path.join(assets_path, "incorrect_sentence_1.txt"),
+            "r",
+            encoding="utf-8"
+        ) as f1,
+        open(
+            os.path.join(assets_path, "incorrect_sentence_2.txt"),
+            "r",
+            encoding="utf-8"
+        ) as f2,
+        open(
+            os.path.join(assets_path, "incorrect_sentence_3.txt"),
+            "r",
+            encoding="utf-8"
+        ) as f3,
+        open(
+            os.path.join(assets_path, "incorrect_sentence_4.txt"),
+            "r",
+            encoding="utf-8"
+        ) as f4,
+        open(
+            os.path.join(assets_path, "incorrect_sentence_5.txt"),
+            "r",
+            encoding="utf-8"
+        ) as f5,
     ):
         sentences = [f.read() for f in (f1, f2, f3, f4, f5)]
 
