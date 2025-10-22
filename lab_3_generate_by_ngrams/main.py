6a833c414ae149dd59cd4c800598f3b64e96fffd
anna_kaizerova <kaizerova.a@yandex.ru>
2025-10-22 23:04:23 +0300

    implementation of get_id, get_token & _put added

--- lab_3_generate_by_ngrams/main.py
+++ lab_3_generate_by_ngrams/main.py
@@ -25,6 +25,7 @@ class TextProcessor:
             end_of_word_token (str): A token denoting word boundary
         """
         self._end_of_word_token = end_of_word_token
+        self._storage = {self._end_of_word_token: 0}
 
     def _tokenize(self, text: str) -> tuple[str, ...] | None:
         """
@@ -75,6 +76,10 @@ class TextProcessor:
         In case of corrupt input arguments or arguments not included in storage,
         None is returned
         """
+        if (not isinstance(element, int)
+            or element not in self._storage):
+            return None
+        return self._storage[element]
 
     def get_end_of_word_token(self) -> str:  # type: ignore[empty-body]
         """
@@ -96,6 +101,10 @@ class TextProcessor:
 
         In case of corrupt input arguments or arguments not included in storage, None is returned
         """
+        if (not isinstance(element_id, int)
+            or element_id not in self._storage):
+            return None
+        return self._storage[element_id]
 
     def encode(self, text: str) -> tuple[int, ...] | None:
         """
@@ -124,6 +133,15 @@ class TextProcessor:
         In case of corrupt input arguments or invalid argument length,
         an element is not added to storage
         """
+        if (not isinstance(element, str)
+            or not element
+            or len(element) != 1):
+            return None
+        if element in self._storage:
+            return self._storage.get(element)
+        new_id = len(self._storage)
+        self._storage = new_id
+        return new_id
 
     def decode(self, encoded_corpus: tuple[int, ...]) -> str | None:
         """
