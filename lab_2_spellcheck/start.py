86c7d90f87e66423e401476b8d9a6fdda4bd1b51
Арина <kretoarina0012@gmail.com>
2025-10-26 18:17:41 +0300

    main

--- lab_2_spellcheck/start.py
+++ lab_2_spellcheck/start.py
@@ -16,16 +16,16 @@ def main() -> None:
     """
     Launches an implementation.
     """
-    with open("assets/Master_and_Margarita_chapter1.txt", "r", encoding="utf-8") as file:
+    with open("lab_2_spellcheck/assets/Master_and_Margarita_chapter1.txt", "r", encoding="utf-8") as file:
         text = file.read()
-    with open("assets/stop_words.txt", "r", encoding="utf-8") as file:
+    with open("lab_2_spellcheck/assets/stop_words.txt", "r", encoding="utf-8") as file:
         stop_words = file.read().split("\n")
     with (
-        open("assets/incorrect_sentence_1.txt", "r", encoding="utf-8") as f1,
-        open("assets/incorrect_sentence_2.txt", "r", encoding="utf-8") as f2,
-        open("assets/incorrect_sentence_3.txt", "r", encoding="utf-8") as f3,
-        open("assets/incorrect_sentence_4.txt", "r", encoding="utf-8") as f4,
-        open("assets/incorrect_sentence_5.txt", "r", encoding="utf-8") as f5,
+        open("lab_2_spellcheck/assets/incorrect_sentence_1.txt", "r", encoding="utf-8") as f1,
+        open("lab_2_spellcheck/assets/incorrect_sentence_2.txt", "r", encoding="utf-8") as f2,
+        open("lab_2_spellcheck/assets/incorrect_sentence_3.txt", "r", encoding="utf-8") as f3,
+        open("lab_2_spellcheck/assets/incorrect_sentence_4.txt", "r", encoding="utf-8") as f4,
+        open("lab_2_spellcheck/assets/incorrect_sentence_5.txt", "r", encoding="utf-8") as f5,
     ):
         sentences = [f.read() for f in (f1, f2, f3, f4, f5)]
     result = None
@@ -39,7 +39,6 @@ def main() -> None:
         print(first_result)
     result = first_result
     assert result, "Result is None"
-
 
 if __name__ == "__main__":
     main()
