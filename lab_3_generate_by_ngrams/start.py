1a7c8c2fbffc9a7ee4e59d3a292ac8715d8eb0da
anna_kaizerova <kaizerova.a@yandex.ru>
2025-10-28 17:41:32 +0300

    file start.py for mark 4

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -3,6 +3,9 @@ Generation by NGrams starter
 """
 
 # pylint:disable=unused-import, unused-variable
+from lab_3_generate_by_ngrams.main import (
+    TextProcessor
+)
 
 
 def main() -> None:
@@ -13,9 +16,13 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-    result = None
+    processor = TextProcessor(end_of_word_token='_')
+    encoded_text = processor.encode(text)
+    print(encoded_text)
+    decoded_text = processor.decode(encoded_text)
+    print(decoded_text)
+    result = decoded_text
     assert result
-
 
 if __name__ == "__main__":
     main()
