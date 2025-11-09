410c6140e7fdabe5e9308988edf3abcf907a637e
annaklimenko-1 <afklimenko@edu.hse.ru>
2025-11-09 15:35:58 +0300

    Add encapsulation seminar code

--- seminars/listing_10_encapsulate.py
+++ seminars/listing_10_encapsulate.py
@@ -29,6 +29,32 @@ class BankAccount:
         validate_transaction(amount: float) -> bool:
             Checks if a transaction amount is valid.
     """
+    def __init__(self, owner: str, balance: float = 0.0):
+        self.owner = owner
+        self._balance = balance
+    
+    def deposit(self, amount: float) -> None:
+        """Adds money to the account."""
+        if self.validate_transaction(amount):
+            self._balance += amount
+    
+    def withdraw(self, amount: float) -> None:
+        """Subtracts money from the account if sufficient funds exist."""
+        if self.validate_transaction(amount) and self._balance >= amount:
+            self._balance -= amount
+    
+    def get_balance(self) -> float:
+        """Returns the current balance of the account."""
+        return self._balance
+    
+    def update_balance(self, amount: float) -> None:
+        """Updates the balance."""
+        if self.validate_transaction(amount):
+            self._balance = amount
+
+    def validate_transaction(self, amount: float) -> bool:
+        """Checks if a transaction amount is valid."""
+        return isinstance(amount, (int, float)) and amount > 0
 
 
 class Bank:
@@ -52,6 +78,20 @@ class Bank:
         generate_account_number() -> int:
             Generates a new account number.
     """
+    def __init__ (self, name: str, accounts: dict, next_account_number: int):
+            self._name = name
+            self.accounts = accounts
+            self.next_number = next_account_number
+
+    def create_account(self, user_name: str):
+        account = {owner_name: BankAccount(owner_name)}
+
+    def generate_account_number(self):
+        self.__next_account_number += 1
+
+    def deposit_to_account(self, account_number, amount):
+        self._accounts[account_number] += amount
+        self.
 
 
 class Person:
@@ -78,6 +118,15 @@ def main() -> None:
     """
     # Work here
     print("Created classes")
+    account = BankAccount('Ksenia', 1000)
+    print(account.get_balance())
+    account.withdraw(500)
+    print(account.get_balance())
+    account.deposit(300)
+    print(account.get_balance())
+    print(account._balance)
+    account._validate_trasaction(-700)
+    print(account.get_balance())
 
 
 if __name__ == "__main__":
