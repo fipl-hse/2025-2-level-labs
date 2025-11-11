4e08a9bd752c89737496519d99c7b84f0cc95c72
Mikhaki <mh4waretgirl@gmail.com>
2025-11-11 09:29:33 +0300

    sem

--- seminars/listing_10_encapsulate.py
+++ seminars/listing_10_encapsulate.py
@@ -1,22 +1,15 @@
 """
 Programming 2025.
-
 Seminar 10.
-
 Encapsulation.
 """
-
 # pylint:disable=too-few-public-methods
-
-
 class BankAccount:
     """
     Represents a bank account.
-
     Instance attributes:
         owner (str): The owner of the account.
         balance (float): The current balance of the account.
-
     Instance methods:
         deposit(amount: float) -> None:
             Adds money to the account.
@@ -29,17 +22,13 @@ class BankAccount:
         validate_transaction(amount: float) -> bool:
             Checks if a transaction amount is valid.
     """
-
-
 class Bank:
     """
     Represents a bank that manages accounts and provides financial services.
-
     Instance attributes:
         name (str): The name of the bank.
         accounts (dict): A dictionary with BankAccount objects.
         next_account_number (int): The next account number to be assigned.
-
     Instance methods:
         create_account(owner_name: str) -> BankAccount:
             Creates a new bank account for a person.
@@ -52,16 +41,12 @@ class Bank:
         generate_account_number() -> int:
             Generates a new account number.
     """
-
-
 class Person:
     """
     Represents a person who can perform financial transactions through their bank account.
-
     Instance attributes:
         name (str): The name of the person.
         account (BankAccount): The person's bank account object.
-
     Instance methods:
         make_purchase(amount: float) -> None:
             Subtracts money from the person's account for a purchase.
@@ -70,8 +55,6 @@ class Person:
         add_funds(amount: float) -> None:
             Adds money to the person's account.
     """
-
-
 def main() -> None:
     """
     Launch listing.
@@ -79,7 +62,5 @@ def main() -> None:
     # Work here
     print("Created classes")
 
-
 if __name__ == "__main__":
     main()
-    
