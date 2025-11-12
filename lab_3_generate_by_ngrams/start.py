69ef4b3e9f59f4d524d3ecae6d918ac85d62761c
Teimur1 <teimurmolodoybrodyaga@gmail.com>
2025-11-13 00:18:30 +0300

    Started my lab#3. It's already fine for mark 4. Will improve it later, of course. RN start.py working fine. Hope u can highlight my mistakes and help to make it better

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -4,6 +4,8 @@ Generation by NGrams starter
 
 # pylint:disable=unused-import, unused-variable
 
+from main import TextProcessor
+
 
 def main() -> None:
     """
@@ -13,8 +15,22 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-    result = None
-    assert result
+
+    processor = TextProcessor ('_')
+
+    tokens = processor._tokenize(text)
+    print("Tokens (sample 50):", tokens[:50] if tokens else None)
+
+    encoded_text = processor.encode(text)
+    print("Encoded (sample 50):", encoded_text[:50] if encoded_text else None)
+
+    decoded_tokens = processor._decode(encoded_text) if encoded_text else None
+    print("Decoded tokens (sample 50):", decoded_tokens[:50] if decoded_tokens else None)
+
+    decoded_text = processor.decode(encoded_text) if encoded_text else None
+    print("Decoded text (first 300 characters):", decoded_text[:300] if decoded_text else None)
+
+    assert decoded_text is not None
 
 
 if __name__ == "__main__":
