1d8792f6fac20471b0208cded4b045ab9c52b711
NatalieSmirnova7 <nataliasmirnova2006@gmail.com>
2025-11-14 21:51:04 +0300

    Code

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -3,7 +3,11 @@ Generation by NGrams starter
 """
 
 # pylint:disable=unused-import, unused-variable
-
+from main import (
+    GreedyTextGenerator,
+    NGramLanguageModel,
+    TextProcessor,
+)
 
 def main() -> None:
     """
@@ -13,6 +17,19 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
+    processor = TextProcessor(end_of_word_token='_')
+    encoded_text = processor.encode(text)
+    if not isinstance(encoded_text, tuple) and not encoded_text:
+        return
+    print(encoded_text)
+    decoded_text = processor.decode(encoded_text)
+    print(decoded_text)
+    model = NGramLanguageModel(encoded_text, 7)
+    frequency = model.build()
+    print(frequency)
+    greedy_generator = GreedyTextGenerator(model, processor)
+    result_greedy_generator = greedy_generator.run(51, "Vernon")
+    print(result_greedy_generator)
     result = None
     assert result
 
