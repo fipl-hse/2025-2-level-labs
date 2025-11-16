6673a9eb9391d7bb50d4a894c04d9e23c276c115
anastasianamakonova <anastasianamakonova@yandex.ru>
2025-11-16 04:11:07 +0300

    added changes for 10

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -3,7 +3,14 @@ Generation by NGrams starter
 """
 
 # pylint:disable=unused-import, unused-variable
-
+from lab_3_generate_by_ngrams.main import (
+    TextProcessor,
+    NGramLanguageModel,
+    GreedyTextGenerator,
+    BeamSearchTextGenerator,
+    NGramLanguageModelReader,
+    BackOffGenerator,
+)
 
 def main() -> None:
     """
@@ -13,7 +20,26 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-    result = None
+    processor = TextProcessor('_')
+    encoded_text = processor.encode(text) or ()
+    decoded_text = processor.decode(encoded_text) or ()
+    model = NGramLanguageModel(encoded_text, 7)
+    model.build()
+    greedy_generator = GreedyTextGenerator(model, processor)
+    greedy_text = greedy_generator.run(51, 'Vernon')
+    print(greedy_text)
+    beam_search_generator = BeamSearchTextGenerator(model, processor, 3)
+    beam_search_text = beam_search_generator.run('Vernon', 56)
+    print(beam_search_text)
+    models = []
+    for ngram_size in (4, 5, 6):
+        model = NGramLanguageModel(encoded_text, ngram_size)
+        model.build()
+        models.append(model)
+    back_off_generator = BackOffGenerator(tuple(models), processor)
+    result_back_off = back_off_generator.run(55, 'Vernon')
+    print(result_back_off)
+    result = result_back_off
     assert result
 
 
