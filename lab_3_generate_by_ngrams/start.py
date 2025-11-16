9569ef0a8768bdd09b53ce6a0987543428607a15
Асташкина Александра <aleksasha.ast@gmail.com>
2025-11-16 22:45:17 +0300

    final newline

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -3,7 +3,8 @@ Generation by NGrams starter
 """
 
 # pylint:disable=unused-import, unused-variable
-from main import (
+from lab_3_generate_by_ngrams.main import (
+    BeamSearchTextGenerator,
     GreedyTextGenerator,
     NGramLanguageModel,
     TextProcessor,
@@ -18,20 +19,25 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-    processor = TextProcessor(end_of_word_token='_')
-    encoded_text = processor.encode(text)
-    if not isinstance(encoded_text, tuple) and not encoded_text:
-        return
-    print(encoded_text)
-    decoded_text = processor.decode(encoded_text)
+    processor = TextProcessor('_')
+    encoded_text = processor.encode(text) or ()
+    
+    decoded_text = processor.decode(encoded_text) or ()
     print(decoded_text)
+    
     model = NGramLanguageModel(encoded_text, 7)
     frequency = model.build()
     print(frequency)
+    
     greedy_generator = GreedyTextGenerator(model, processor)
     result_greedy_generator = greedy_generator.run(51, "Vernon")
     print(result_greedy_generator)
-    result = result_greedy_generator
+    
+    beam_search_generator = BeamSearchTextGenerator(model, processor, 3)
+    result_beam_search = beam_search_generator.run('Vernon', 56)
+    print(result_beam_search)
+    
+    result = result_beam_search
     assert result
 
 
