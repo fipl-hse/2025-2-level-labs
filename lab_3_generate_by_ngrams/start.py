223acd3926fe138808df231cbd9f397a8dcd1940
annaklimenko-1 <afklimenko@edu.hse.ru>
2025-11-16 15:47:15 +0300

    start

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -3,6 +3,7 @@ Generation by NGrams starter
 """
 
 # pylint:disable=unused-import, unused-variable
+from lab_3_generate_by_ngrams import TextProcessor, NGramLanguageModel,  GreedyTextGenerator
 
 
 def main() -> None:
@@ -13,6 +14,26 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
+    
+    processor = TextProcessor("_")
+    
+    encoded = processor.encode(text)
+    
+    if encoded:
+        decoded = processor.decode(encoded[:1000])
+    
+    prompt = "Vernon"
+    n_gram_size = 7
+    seq_len = 51
+    
+    if encoded:
+        model = NGramLanguageModel(encoded, n_gram_size)
+        build_result = model.build()
+        
+        if build_result == 0:
+            generator = GreedyTextGenerator(model, processor)
+            result = generator.run(seq_len, prompt)
+    
     result = None
     assert result
 
