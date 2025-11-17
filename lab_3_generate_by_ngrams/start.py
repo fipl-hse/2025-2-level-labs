25d761bbea72bf32c5584ed3baa5ba820f49ed3e
Olga <olgakuznetsova006@gmail.com>
2025-11-17 07:46:25 +0300

    change start

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -38,12 +38,12 @@ def main() -> None:
 
     language_models = []
     for n_gram_size in [1, 2, 3]:
-        model = NGramLanguageModelReader("./assets/en_own.json", "_").load(n_gram_size)
-        if model is not None:
+        loaded_model = NGramLanguageModelReader("./assets/en_own.json", "_").load(n_gram_size)
+        if loaded_model is not None:
             language_models.append(model)
 
     back_off = BackOffGenerator(tuple(language_models), processor).run(60, 'Vernon')
-    result = back_off 
+    result = back_off
     print(result)
     assert result
 
