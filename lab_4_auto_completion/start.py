fc2767c30249facbd6d3b2ae55de080568ff2aa5
Yuztrazure <artemijsidnev04923@gmail.com>
2025-11-19 18:22:28 +0300

    secret realization. But there should be something else

--- lab_4_auto_completion/start.py
+++ lab_4_auto_completion/start.py
@@ -3,7 +3,8 @@ Auto-completion start
 """
 
 # pylint:disable=unused-variable
-
+from lab_3_generate_by_ngrams.main import BeamSearchTextGenerator, NGramLanguageModel, TextProcessor
+from lab_4_auto_completion.main import WordProcessor, TextProcessingError
 
 def main() -> None:
     """
@@ -15,7 +16,36 @@ def main() -> None:
         hp_letters = letters_file.read()
     with open("./assets/ussr_letters.txt", "r", encoding="utf-8") as text_file:
         ussr_letters = text_file.read()
-    result = None
+
+    with open("./assets/secrets/secret_1.txt", "r", encoding="utf=8") as secret_file:
+        secret_text = secret_file.read()
+    with open("../lab_3_generate_by_ngrams/assets/Harry_Potter.txt", "r", encoding="utf=8") as harry_file:
+        harry_text = harry_file.read()
+
+    word_processor = TextProcessor("<EoW>")
+    try:
+        n_gram_size = 15
+        beam_width = 3
+        seq_len = 100
+
+        parts = secret_text.split("<BURNED>")
+
+        context_before = parts[0].strip().replace("\n", " ")
+        context_after = parts[1].replace("\n", " ")
+        context_for_generation = context_before[-n_gram_size:]
+
+        encoded_secret = word_processor.encode(harry_text)
+        n_gram_model = NGramLanguageModel(tuple(encoded_secret), n_gram_size)
+        n_gram_model.build()
+
+        beam_generator = BeamSearchTextGenerator(n_gram_model, word_processor, beam_width)
+        output_beam: str | None = beam_generator.run(context_for_generation, seq_len)
+        recovered_letter = f"{context_before[:-n_gram_size]}{output_beam} {context_after}"
+    except TextProcessingError:
+        print("Fail to process text")
+
+    print(recovered_letter)
+    result = recovered_letter
     assert result, "Result is None"
 
 
