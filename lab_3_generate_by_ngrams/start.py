cc03f59b3ca88163b2873440e34d00d218dc52ff
ESverchkova <easverchkova@edu.hse.ru>
2025-11-20 15:33:07 +0300

    git asked me to do a commit before pulling

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -23,41 +23,30 @@ def main() -> None:
         text = text_file.read()
     processor = TextProcessor(end_of_word_token='_')
     encoded = processor.encode(text) or tuple()
-    reader = NGramLanguageModelReader('./assets/en_own.json', '_')
     if encoded:
-        custom_model = NGramLanguageModel(encoded, 4)
-        custom_model.build()
-        custom_result = GreedyTextGenerator(
-            custom_model,
-            processor
-        ).run(15, 'Harry')
-        print(f"\nCustom model from Harry Potter: {custom_result}")
-    loaded_model_list = []
-    for n_size in (2, 3, 4, 5, 6):
-        loaded_model = reader.load(n_size)
-        if loaded_model is not None:
-            if n_size <= 5:
-                result = GreedyTextGenerator(
-                    loaded_model,
-                    reader.get_text_processor()
-                ).run(20, 'Harry')
-                print(f"\nGreedy with loaded {n_size}-gram: {result}")
-            if n_size >= 4:
-                result = BeamSearchTextGenerator(
-                    loaded_model,
-                    reader.get_text_processor(),
-                    2
-                ).run('Hermione', 25)
-                print(f"\nBeam Search with loaded {n_size}-gram: {result}")
-            if n_size >= 3:
-                loaded_model_list.append(loaded_model)
-    if loaded_model_list:
-        result = BackOffGenerator(
-            tuple(loaded_model_list),
-            reader.get_text_processor()
-        ).run(30, 'Dumbledore')
-        print(f"\nBackOff with loaded models: {result}")
-        assert result
+        loaded_model_list = []
+        for n_size in range(2, 11):
+            custom_model = NGramLanguageModel(encoded, n_size)
+            custom_model.build()
+            loaded_model_list.append(custom_model)
+            result = GreedyTextGenerator(
+                custom_model,
+                processor
+            ).run(30, 'Harry')
+            print(f"\nGreedy with custom {n_size}-gram: {result}")
+            result = BeamSearchTextGenerator(
+                custom_model,
+                processor,
+                2
+            ).run('Harry', 30)
+            print(f"\nBeam Search with custom {n_size}-gram: {result}")
+        if loaded_model_list:
+            result = BackOffGenerator(
+                tuple(loaded_model_list),
+                processor
+            ).run(35, 'Harry')
+            print(f"\nBackOff with all custom models: {result}")
+            assert result
 
 
 if __name__ == "__main__":
