149fda7f529eb8dd9400ab7b24db6dcaacb269e6
Kurayto <balaganinamasha@yandex.ru>
2025-11-30 22:26:50 +0300

    version3_8

--- seminars/listing_11_inheritance.py
+++ seminars/listing_11_inheritance.py
@@ -21,14 +21,6 @@ class Vehicle:
         move() -> None:
             Simulates vehicle movement.
     """
-    def __init__(self, colour: str, max_speed: int):
-        self.colour = colour
-        self.max_speed = max_speed
-
-    def move(self) -> None:
-        print(f"vehicles is moving")
-       
-
 
 
 class Car:
@@ -46,16 +38,6 @@ class Car:
         stay() -> None:
             Simulates stopping the car.
     """
-    def __init__(self, max_speed: int, colour: str, fuel: str):
-        Vehicle.__init__(self, max_speed, colour)
-        self.fuel = fuel
-
-    def move(self) -> None:
-        print(f"{self.fuel} car is moving")
-    
-    def stay(self) -> None:
-        print(f"car is staying")
-
 
 
 class Bicycle:
@@ -73,15 +55,7 @@ class Bicycle:
         freestyle() -> None:
             Simulates performing a freestyle trick.
     """
-    def __init__(self, number_of_wheels: int, colour: str, max_speed: int):
-        Vehicle.__init__(self, max_speed, colour)
-        self.number_of_wheels = number_of_wheels
 
-    def move(self) -> None:
-        print(f"{self.number_of_wheels} bicycle is moving")
-              
-    def freestyle(self) -> None:
-        print(f"biciycle is making freestyle")
 
 class Aircraft:
     """
@@ -96,29 +70,14 @@ class Aircraft:
         move() -> None:
             Simulates aircraft movement.
     """
-    def __init__(self, number_of_engines: int, colour: str, max_speed: int):
-        Vehicle.__init__(self, max_speed, colour)
-        self.number_of_engines = number_of_engines
-
-    def move(self) -> None:
-        print(f"{self.number_of_engines} aircraft is moving")
 
 
 def main() -> None:
     """
     Launch listing.
     """
-    car = Car(180, "red", 100)
-    bicycle = Bicycle(30, "blue", 2)
-    aircraft = Aircraft(900, "white", 4)
-    vehicles = [car, bicycle, aircraft]
-    
-    for i in vehicles:
-        i.move()
-
-    car.stay()
-    bicycle.freestyle()
     print("Created inheritance hierarchy: Vehicle -> Car, Bicycle, Aircraft")
+
 
 if __name__ == "__main__":
     main()
