7f209a4aa2817937ccef1e249b927355a51f9ec5
Ksenia <ilovehotpupik@internet.ru>
2025-12-03 00:36:27 +0300

    import fixed

--- lab_4_auto_completion/start.py
+++ lab_4_auto_completion/start.py
@@ -3,8 +3,14 @@ Auto-completion start
 """
 
 # pylint:disable=unused-variable
-from lab_3_generate_by_ngrams.main import BeamSearchTextGenerator, GreedyTextGenerator, NGramLanguageModel, TextProcessor
-from lab_4_auto_completion.main import WordProcessor, NGramTrieLanguageModel
+from lab_3_generate_by_ngrams.main import (
+    BeamSearchTextGenerator,
+    GreedyTextGenerator,
+    NGramLanguageModel,
+    TextProcessor,
+)
+from lab_4_auto_completion.main import NGramTrieLanguageModel, WordProcessor
+
 
 def main() -> None:
     """
