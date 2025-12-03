8e5ecac9f73499729577b1c1a5967d9812d8f9a9
Jonquilot <steshovaasa@gmail.com>
2025-12-03 22:01:57 +0300

    _put function realized

--- lab_3_generate_by_ngrams/main.py
+++ lab_3_generate_by_ngrams/main.py
@@ -121,6 +121,11 @@ class TextProcessor:
         In case of corrupt input arguments or invalid argument length,
         an element is not added to storage
         """
+        if not isinstance(element, str) or len(element) > 0:
+            return None
+        if not element in self._storage:
+            self._storage[element] = len(self._storage)
+        return None
 
     def decode(self, encoded_corpus: tuple[int, ...]) -> str | None:
         """
