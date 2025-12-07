1a80c4a8f20e353284552db156ece32eced4085a
anna_kaizerova <kaizerova.a@yandex.ru>
2025-12-07 16:46:40 +0300

    fixing ci

--- lab_4_auto_completion/start.py
+++ lab_4_auto_completion/start.py
@@ -3,7 +3,7 @@ Auto-completion start
 """
 
 # pylint:disable=unused-variable
-from lab_3_generate_by_ngrams.main import BeamSearcher, BeamSearchTextGenerator, GreedyTextGenerator
+from lab_3_generate_by_ngrams.main import BeamSearchTextGenerator, GreedyTextGenerator
 from lab_4_auto_completion.main import (
     DynamicBackOffGenerator,
     DynamicNgramLMTrie,
@@ -26,8 +26,6 @@ def main() -> None:
         hp_letters = letters_file.read()
     with open("./assets/ussr_letters.txt", "r", encoding="utf-8") as text_file:
         ussr_letters = text_file.read()
-    with open("./assets/secrets/secret_5.txt", "r", encoding="utf-8") as text_file:
-        letter = text_file.read()
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as harry_file:
         text = harry_file.read()
     processor = WordProcessor('<EoS>')
@@ -38,37 +36,29 @@ def main() -> None:
     suggestions = trie.suggest((2,))
     if suggestions:
         first = suggestions[0]
-        words = []
-        for code in first:
-            for text, el in processor._storage.items():
-                if el == code:
-                    words.append(text)
-                    break
-    decoded_text = processor._postprocess_decoded_text(tuple(words))
-    print(f"\n1. Decoded result: {decoded_text}")
+        print(f"\n1. Decoded result: {processor.decode(first)}")
 
     model = NGramTrieLanguageModel(hp_encoded, 5)
     model.build()
 
-    greedy_result = GreedyTextGenerator(model, processor).run(52, 'Harry Potter')
+    greedy_result = GreedyTextGenerator(model, processor).run(52, 'Dear')
     print(f"\n2. Greedy result before: {greedy_result}")
-    beam_result = BeamSearchTextGenerator(model, processor, 3).run('Harry Potter', 52)
+    beam_result = BeamSearchTextGenerator(model, processor, 3).run('Dear', 52)
     print(f"Beam result before: {beam_result}")
 
     encoded_ussr = processor.encode_sentences(ussr_letters)
     model.update(encoded_ussr)
 
-    greedy_updated = GreedyTextGenerator(model, processor).run(52, 'Harry Potter')
+    greedy_updated = GreedyTextGenerator(model, processor).run(52, 'Dear')
     print(f"\n3. Greedy result after: {greedy_updated}")
-    beam_updated = BeamSearchTextGenerator(model, processor, 3).run('Harry Potter', 52)
+    beam_updated = BeamSearchTextGenerator(model, processor, 3).run('Dear', 52)
     print(f"Beam result before: {beam_updated}")
 
     dynamic_trie = DynamicNgramLMTrie(hp_encoded, 5)
     dynamic_trie.build()
 
-    save_path = "./saved_dynamic_trie.json"
-    save(dynamic_trie, save_path)
-    loaded_trie = load(save_path)
+    save(dynamic_trie, "./saved_dynamic_trie.json")
+    loaded_trie = load("./saved_dynamic_trie.json")
 
     dynamic_generator = DynamicBackOffGenerator(loaded_trie, processor)
     dynamic_result_before = dynamic_generator.run(50, 'Ivanov')
