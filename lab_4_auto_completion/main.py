c2e19c0cf559abffd55991edea7db4d834e8e0d7
SletA <sletanimatics@gmail.com>
2025-12-07 23:33:02 +0300

    newCommitInToGoingOnNinete

--- lab_4_auto_completion/main.py
+++ lab_4_auto_completion/main.py
@@ -197,7 +197,7 @@ class TrieNode:
     #: Children nodes
     _children: list["TrieNode"]
 
-    def __init__(self,name: int | None = None, value: float = 0.0, 
+    def __init__(self,name: int | None = None, value: float = 0.0,
                  children: list["TrieNode"] | None = None,
         ) -> None:
         """
