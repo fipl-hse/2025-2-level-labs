6de23d2d5302670142001462cb16ed63c8d84963
Olga <olgakuznetsova006@gmail.com>
2025-12-07 22:06:17 +0300

    too many locals

--- lab_4_auto_completion/start.py
+++ lab_4_auto_completion/start.py
@@ -22,8 +22,9 @@ def main() -> None:
     prefix_trie = PrefixTrie()
     prefix_trie.fill(encoded_sentences)
     suggestions = prefix_trie.suggest((2,))
-    decoded_suggestion = processor.decode(suggestions[0])
-    print(decoded_suggestion)
+    if suggestions:
+        processor.decode(suggestions[0])
+        print(f"Первое продолжение для префикса 2: {processor.decode(suggestions[0])}")
 
     model = NGramTrieLanguageModel(encoded_sentences, 5)
     model.build()
@@ -31,30 +32,27 @@ def main() -> None:
     greedy_generator = GreedyTextGenerator(model, processor)
     beam_generator = BeamSearchTextGenerator(model, processor, 5)
 
-    start_context = "Harry Potter"
+    print("Жадный алгоритм до обновления:")
+    greedy_result_before = greedy_generator.run(15, "Harry Potter")
+    print(f"Результат: {greedy_result_before}")
 
-    print("Жадный алгоритм до обновления")
-    greedy_result_before = greedy_generator.run(15, start_context)
-    print(greedy_result_before)
+    print("Beam Search до обновления:")
+    beam_result_before = beam_generator.run("Harry Potter", 15)
+    print(f"Результат: {beam_result_before}")
 
-    print("Beam Search до обновления")
-    beam_result_before = beam_generator.run(start_context, 15)
-    print(beam_result_before)
+    model.update(processor.encode_sentences(ussr_letters))
 
-    ussr_encoded = processor.encode_sentences(ussr_letters)
-    model.update(ussr_encoded)
-
-    print("Жадный алгоритм после обновления")
-    greedy_result_after = greedy_generator.run(15, start_context)
+    print("Жадный алгоритм после обновления:")
+    greedy_result_after = greedy_generator.run(15, "Harry Potter")
     print(greedy_result_after)
 
-    print("Beam Search после обновления")
-    beam_result_after = beam_generator.run(start_context, 15)
-    print(beam_result_after)
+    print("Beam Search (beam_width=5) (после обновления):")
+    beam_result_after = beam_generator.run("Harry Potter", 15)
+    print(f"Результат: {beam_result_after}")
 
     result = greedy_result_before
     print(f"Итоговый результат: {result}")
-    
+
     print(result)
     assert result, "Result is None"
 
