793ccd4f31d82e2bed079d156fe73f5c2756c73d
annaklimenko-1 <afklimenko@edu.hse.ru>
2025-12-07 23:00:32 +0300

    start check

--- lab_4_auto_completion/start.py
+++ lab_4_auto_completion/start.py
@@ -29,8 +29,6 @@ def main() -> None:
         hp_letters = letters_file.read()
     with open("./assets/ussr_letters.txt", "r", encoding="utf-8") as text_file:
         ussr_letters = text_file.read()
-    result = None
-    assert result, "Result is None"
 
     processor = WordProcessor('<EOS>')
     encoded_sentences = processor.encode_sentences(hp_letters)
