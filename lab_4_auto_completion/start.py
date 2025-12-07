6aa6783b663c1185f85cea3e08bf3cd99162a432
Mikhaki <mh4waretgirl@gmail.com>
2025-12-07 19:46:51 +0300

    edited start.py

--- lab_4_auto_completion/start.py
+++ lab_4_auto_completion/start.py
@@ -19,35 +19,37 @@ def main() -> None:
     with open("./assets/ussr_letters.txt", "r", encoding="utf-8") as text_file:
         ussr_letters = text_file.read()
 
-    n_gram_size = 5
-    beam_width = 3
-    seq_len = 51
     word_processor = WordProcessor("<EoS>")
     hp_encoded_text = word_processor.encode_sentences(hp_letters)
     prefix_trie = PrefixTrie()
     prefix_trie.fill(hp_encoded_text)
     suggestions = prefix_trie.suggest((2,))
     if suggestions:
-        decoded_suggestions = word_processor.decode(suggestions[0])
-        print("For 6: ", decoded_suggestions.replace("<EoS>", "").strip())
+        print("For 6: ", word_processor.decode(suggestions[0]).replace("<EoS>", "").strip())
 
 
-    ngram_trie_lm = NGramTrieLanguageModel(hp_encoded_text, n_gram_size)
+    ngram_trie_lm = NGramTrieLanguageModel(hp_encoded_text, 5)
     ngram_trie_lm.build()
 
     ussr_encoded_text = word_processor.encode_sentences(ussr_letters)
 
     greedy_generator = GreedyTextGenerator(ngram_trie_lm, word_processor)
-    beamsearcher = BeamSearchTextGenerator(ngram_trie_lm, word_processor, beam_width)
+    beamsearcher = BeamSearchTextGenerator(ngram_trie_lm, word_processor, 3)
 
-    ussr_corp_before = (greedy_generator.run(seq_len=30, prompt="Dear"), beamsearcher.run(prompt="Dear", seq_len=30))
+    ussr_corp_before = (
+        greedy_generator.run(seq_len=30, prompt="Dear"),
+        beamsearcher.run(prompt="Dear", seq_len=30)
+        )
     print(f"Greedy before update = {ussr_corp_before[0]}, Beam = {ussr_corp_before[1]}")
     print()
     # print("Beamsearcher before update", ussr_corp_beam)
     print()
 
     ngram_trie_lm.update(ussr_encoded_text)
-    ussr_corp_after = (greedy_generator.run(seq_len=30, prompt="Dear"), beamsearcher.run(prompt="Dear", seq_len=30))
+    ussr_corp_after = (
+        greedy_generator.run(seq_len=30, prompt="Dear"),
+        beamsearcher.run(prompt="Dear", seq_len=30)
+        )
     print(f"Greedy after update = {ussr_corp_before[0]}, Beam = {ussr_corp_before[1]}")
     print()
 
