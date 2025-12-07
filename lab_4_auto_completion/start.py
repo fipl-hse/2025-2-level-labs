3dcb99240a216115215f2b1c2643e815009aa489
anna_kaizerova <kaizerova.a@yandex.ru>
2025-12-07 17:03:45 +0300

    fixing ci

--- lab_4_auto_completion/start.py
+++ lab_4_auto_completion/start.py
@@ -26,23 +26,18 @@ def main() -> None:
         hp_letters = letters_file.read()
     with open("./assets/ussr_letters.txt", "r", encoding="utf-8") as text_file:
         ussr_letters = text_file.read()
-    with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as harry_file:
-        text = harry_file.read()
     processor = WordProcessor('<EoS>')
     hp_encoded = processor.encode_sentences(hp_letters)
 
     trie = PrefixTrie()
     trie.fill(hp_encoded)
-    suggestions = trie.suggest((2,))
-    if suggestions:
-        first = suggestions[0]
-        print(f"\n1. Decoded result: {processor.decode(first)}")
+    suggestion = trie.suggest((2,))[0]
+    print(f"\n1. Decoded result: {processor.decode(suggestion)}")
 
     model = NGramTrieLanguageModel(hp_encoded, 5)
     model.build()
 
-    greedy_result = GreedyTextGenerator(model, processor).run(52, 'Dear')
-    print(f"\n2. Greedy result before: {greedy_result}")
+    print(f"\n2. Greedy result before: {GreedyTextGenerator(model, processor).run(52, 'Dear')}")
     beam_result = BeamSearchTextGenerator(model, processor, 3).run('Dear', 52)
     print(f"Beam result before: {beam_result}")
 
@@ -61,8 +56,7 @@ def main() -> None:
     loaded_trie = load("./saved_dynamic_trie.json")
 
     dynamic_generator = DynamicBackOffGenerator(loaded_trie, processor)
-    dynamic_result_before = dynamic_generator.run(50, 'Ivanov')
-    print(f"\n4. Dynamic result before: {dynamic_result_before}")
+    print(f"\n4. Dynamic result before: {dynamic_generator.run(50, 'Ivanov')}")
 
     loaded_trie.update(encoded_ussr)
     loaded_trie.set_current_ngram_size(3)
@@ -71,10 +65,9 @@ def main() -> None:
     except IncorrectNgramError:
         loaded_trie.set_current_ngram_size(None)
 
-    dynamic_result_after = dynamic_generator.run(50, 'Ivanov')
-    print(f"Dynamic result after: {dynamic_result_after}\n")
+    print(f"Dynamic result after: {dynamic_generator.run(50, 'Ivanov')}\n")
 
-    result = dynamic_result_after
+    result = dynamic_generator
     assert result, "Result is None"
 
 if __name__ == "__main__":
