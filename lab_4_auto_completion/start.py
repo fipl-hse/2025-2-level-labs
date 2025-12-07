cc3fc4abb516040a2eca62d1762218bfaae4057b
Olga <olgakuznetsova006@gmail.com>
2025-12-07 21:59:38 +0300

    st 8

--- lab_4_auto_completion/start.py
+++ lab_4_auto_completion/start.py
@@ -23,20 +23,38 @@ def main() -> None:
     prefix_trie.fill(encoded_sentences)
     suggestions = prefix_trie.suggest((2,))
     decoded_suggestion = processor.decode(suggestions[0])
-    #result = decoded_suggestion
+    print(decoded_suggestion)
 
     model = NGramTrieLanguageModel(encoded_sentences, 5)
     model.build()
 
+    greedy_generator = GreedyTextGenerator(model, processor)
+    beam_generator = BeamSearchTextGenerator(model, processor, 5)
+
+    start_context = "Harry Potter"
+
+    print("Жадный алгоритм до обновления")
+    greedy_result_before = greedy_generator.run(15, start_context)
+    print(greedy_result_before)
+
+    print("Beam Search до обновления")
+    beam_result_before = beam_generator.run(start_context, 15)
+    print(beam_result_before)
+
     ussr_encoded = processor.encode_sentences(ussr_letters)
     model.update(ussr_encoded)
 
-    greedy_generator = GreedyTextGenerator(model, processor)
-    beam_generator = BeamSearchTextGenerator(model, processor, 3)
+    print("Жадный алгоритм после обновления")
+    greedy_result_after = greedy_generator.run(15, start_context)
+    print(greedy_result_after)
 
-    result_generator = greedy_generator.run(51, "Harry ")
-    print(result_generator)
-    result = None
+    print("Beam Search после обновления")
+    beam_result_after = beam_generator.run(start_context, 15)
+    print(beam_result_after)
+
+    result = greedy_result_before
+    print(f"Итоговый результат: {result}")
+    
     print(result)
     assert result, "Result is None"
 
