3f5f78cb5d7db036324cc28c6feae8fe33623b56
Yuztrazure <artemijsidnev04923@gmail.com>
2025-12-08 14:01:17 +0300

    making CI green

--- lab_4_auto_completion/start.py
+++ lab_4_auto_completion/start.py
@@ -3,11 +3,6 @@ Auto-completion start
 """
 
 # pylint:disable=unused-variable
-from lab_3_generate_by_ngrams.main import (
-    BackOffGenerator,
-    BeamSearchTextGenerator,
-    GreedyTextGenerator
-)
 from lab_4_auto_completion.main import (
     DynamicBackOffGenerator,
     DynamicNgramLMTrie,
@@ -53,22 +48,6 @@ def main() -> None:
     loaded_model.update(encoded_corpus)
 
     print(f"Dynamic BackOFF with 2 corpuses: {generator.run(seq_len, prompt)}")
-
-    encoded_corpus = processor.encode_sentences(hp_letters + ussr_letters)
-    language_model = NGramTrieLanguageModel(encoded_corpus, 5)
-    language_model.build()
-
-    # backoff = BackOffGenerator(language_model, processor)
-    beamsearch = BeamSearchTextGenerator(language_model, processor, 5)
-    greedy = GreedyTextGenerator(language_model, processor)
-
-    # backoff_out = backoff.run(seq_len, prompt)
-    beamsearch_out = beamsearch.run(prompt, seq_len)
-    greedy_out = greedy.run(seq_len, prompt)
-
-    # print(f"Backoff: {backoff_out}")
-    print(f"BeamSearch: {beamsearch_out}")
-    print(f"Greedy: {greedy_out}")
 
     result = loaded_model
     assert result
