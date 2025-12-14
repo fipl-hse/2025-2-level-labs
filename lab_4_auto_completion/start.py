0023b257dd1e9c8f4692a55cdb81e7d1c1516d43
AimlesslyWalking <dddavtian@edu.hse.ru>
2025-12-14 22:01:10 +0300

    Shortened some lines

--- lab_4_auto_completion/start.py
+++ lab_4_auto_completion/start.py
@@ -39,16 +39,17 @@ def main() -> None:
     lm = NGramTrieLanguageModel(hp_encoded, 5)
     lm.build()
 
-    print(f"\n2.Greedy result before merging: {GreedyTextGenerator(lm, processor).run(52, 'Dear')}")
-    print(f"Beam result before merging: {BeamSearchTextGenerator(lm, processor, 3).run('Dear', 52)}")
+    print(f"\n 2.Greedy before merging: {GreedyTextGenerator(lm, processor).run(52, 'Dear')}")
+    print(f"Beam before merging: {BeamSearchTextGenerator(lm, processor, 3).run('Dear', 52)}")
 
     print("\nMerging corpuses...")
     encoded_ussr = processor.encode_sentences(ussr_letters)
     lm.update(encoded_ussr)
 
-    print(f"\n3.Greedy result after merging: {GreedyTextGenerator(lm, processor).run(52, 'Dear')}")
+    print(f"\n3.Greedy after merging: {GreedyTextGenerator(lm, processor).run(52, 'Dear')}")
     beam_updated = BeamSearchTextGenerator(lm, processor, 3).run('Dear', 52)
-    print(f"Beam result after merging: {beam_updated}")
+    print(f"Beam after merging: {beam_updated}")
+
 
     dynamic_trie = DynamicNgramLMTrie(hp_encoded, 5)
     dynamic_trie.build()
@@ -57,7 +58,7 @@ def main() -> None:
     loaded_trie = load(r"./assets/dynamic_trie.json")
 
     dynamic_generator = DynamicBackOffGenerator(loaded_trie, processor)
-    print(f"\n4. Dynamic result before merging: {dynamic_generator.run(50, 'Ivanov')}")
+    print(f"\n4. Dynamic back off before merging: {dynamic_generator.run(50, 'Ivanov')}")
 
     loaded_trie.update(encoded_ussr)
     loaded_trie.set_current_ngram_size(3)
@@ -66,7 +67,7 @@ def main() -> None:
     except IncorrectNgramError:
         loaded_trie.set_current_ngram_size(None)
 
-    print(f"Dynamic result after merging: {dynamic_generator.run(50, 'Ivanov')}\n")
+    print(f"Dynamic back off after merging: {dynamic_generator.run(50, 'Ivanov')}\n")
 
     result = dynamic_generator
     assert result, "Result is None"
